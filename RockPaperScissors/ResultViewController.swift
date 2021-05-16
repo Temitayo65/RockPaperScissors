af449618232f3b56ed25d1fff4ef0c86807c96fd
temitayo65 <iloritemitayo65@gmail.com>
2021-05-16 03:08:21 +0100

    Resolving Merge Conflict

--- RockPaperScissors/ResultViewController.swift
+++ RockPaperScissors/ResultViewController.swift
@@ -16,6 +16,7 @@ class ResultViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        view.backgroundColor = #colorLiteral(red: 0.5568627715, green: 0.3529411852, blue: 0.9686274529, alpha: 1)
         // Do any additional setup after loading the view.
     }
     
